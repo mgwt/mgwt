fcba08c5ed05c321a2ac7d7dd0f714fdeee0cc16
Katharina Fahnenbruck <katharina.fahnenbruck@gmail.com>
2014-08-01 20:51:26 +0200

    Added iOS theming for SearchBox.

--- src/main/java/com/googlecode/mgwt/ui/client/theme/platform/input/search/MSearchBoxIOSAppearance.java
+++ src/main/java/com/googlecode/mgwt/ui/client/theme/platform/input/search/MSearchBoxIOSAppearance.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2014 Daniel Kurka
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -19,7 +19,7 @@ import com.google.gwt.resources.client.DataResource;
 
 import com.googlecode.mgwt.ui.client.widget.input.search.MSearchBoxAbstractAppearance;
 
-public class MSearchBoxIPadAppearance extends MSearchBoxAbstractAppearance {
+public class MSearchBoxIOSAppearance extends MSearchBoxAbstractAppearance {
 
   static {
     Resources.INSTANCE.css().ensureInjected();
@@ -29,7 +29,7 @@ public class MSearchBoxIPadAppearance extends MSearchBoxAbstractAppearance {
 
     Resources INSTANCE = GWT.create(Resources.class);
 
-    @Source({"searchbox-base.css", "searchbox-ipad.css"})
+    @Source({"com/googlecode/mgwt/ui/client/widget/input/search/searchbox.css", "searchbox-ios.css"})
     MSearchBoxCss css();
 
     @Source("glass.png")
